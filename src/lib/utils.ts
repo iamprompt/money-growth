9205dbb1897c14d5bc422034106a86b184409698
Supakarn Laorattanakul <wearedprompt@gmail.com>
2024-07-16 14:23:04 +0700

    fix: add utils roundValue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from 'clsx'
+import { round } from 'lodash'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const roundValue = (value: number) => {
+  return round(round(value, 3), 2)
 }
