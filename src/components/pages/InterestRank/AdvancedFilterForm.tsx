b13d5c95b92055b526756612726ceeec4a703ceb
Supakarn Laorattanakul <wearedprompt@gmail.com>
2024-07-20 03:04:36 +0700

    feat: add bonus condition tooltip

--- src/components/pages/InterestRank/AdvancedFilterForm.tsx
+++ src/components/pages/InterestRank/AdvancedFilterForm.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Accordion } from '@radix-ui/react-accordion'
+import { Info } from 'lucide-react'
 import Image from 'next/image'
 import {
   forwardRef,
@@ -35,6 +36,12 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet'
 import { Switch } from '@/components/ui/switch'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 import { accounts, bonusAccountLists } from '@/data/accounts'
 import { BankCode, banks } from '@/data/banks'
 import { useMediaQuery } from '@/hooks/useMediaQuery'
@@ -200,174 +207,188 @@ const AdvancedFilterForm = forwardRef<
   }, [accountPrefs])
 
   return (
-    <div className={cn(className)}>
-      <div>
+    <TooltipProvider>
+      <div className={cn(className)}>
         <div>
-          <h3 className="text-lg font-semibold mb-4">
-            บัญชีเงินฝากดอกเบี้ยสูง
-          </h3>
-        </div>
-        <Accordion
-          type="multiple"
-          value={openPanels}
-          onValueChange={setOpenPanels}
-        >
-          {Object.entries(accounts).map(([bankCode, accounts]) => {
-            if (!accounts.length) return null
+          <div>
+            <h3 className="text-lg font-semibold mb-4">
+              บัญชีเงินฝากดอกเบี้ยสูง
+            </h3>
+          </div>
+          <Accordion
+            type="multiple"
+            value={openPanels}
+            onValueChange={setOpenPanels}
+          >
+            {Object.entries(accounts).map(([bankCode, accounts]) => {
+              if (!accounts.length) return null
 
-            const bank = banks[bankCode as BankCode]
-            const isBankEnable = accounts.some(
-              (account) =>
-                accountPreferences.get(account.code)?.enabled ?? true,
-            )
+              const bank = banks[bankCode as BankCode]
+              const isBankEnable = accounts.some(
+                (account) =>
+                  accountPreferences.get(account.code)?.enabled ?? true,
+              )
 
-            return (
-              <AccordionItem
-                className="border-none"
-                key={`advanced_filter_${bankCode}`}
-                value={`advanced_filter_${bankCode}`}
-              >
-                <AccordionTrigger className="p-2 flex gap-2 border-b">
-                  <div className="flex items-center gap-2">
-                    <div
-                      className={cn(
-                        'size-6 rounded-full border shrink-0',
-                        !isBankEnable && 'grayscale',
-                      )}
-                      style={{
-                        backgroundColor: bank.icon?.bgColor,
-                      }}
-                    >
-                      {bank.icon && (
-                        <Image
-                          src={bank.icon.path}
-                          alt={bank.nameTh}
-                          width={40}
-                          height={40}
-                        />
-                      )}
-                    </div>
-                    <div
-                      className={cn(
-                        'flex flex-col',
-                        !isBankEnable && 'text-gray-500',
-                      )}
-                    >
-                      <span className="font-medium">{bank.nameTh}</span>
-                    </div>
-                  </div>
-                </AccordionTrigger>
-                <AccordionContent className="p-0">
-                  {accounts.map((account) => {
-                    const accountPref = accountPreferences.get(account.code)
-
-                    return (
-                      <div
-                        key={account.code}
-                        className="p-2 flex justify-between items-center"
-                      >
-                        <label
-                          htmlFor={`account-switch-${account.code}`}
-                          className={
-                            (accountPref?.enabled ?? true)
-                              ? 'text-primary'
-                              : 'text-gray-500'
-                          }
-                        >
-                          {account.shortName || account.name}
-                        </label>
-                        <div>
-                          <Switch
-                            id={`account-switch-${account.code}`}
-                            checked={accountPref?.enabled ?? true}
-                            onCheckedChange={(checked) => {
-                              if (!accountPref) {
-                                setAccountPreferences(
-                                  new Map(
-                                    accountPreferences.set(account.code, {
-                                      productCode: account.code,
-                                      enabled: checked,
-                                    }),
-                                  ),
-                                )
-                              } else {
-                                setAccountPreferences(
-                                  new Map(
-                                    accountPreferences.set(account.code, {
-                                      ...accountPref,
-                                      enabled: checked,
-                                    }),
-                                  ),
-                                )
-                              }
-                            }}
-                          />
-                        </div>
-                      </div>
-                    )
-                  })}
-                </AccordionContent>
-              </AccordionItem>
-            )
-          })}
-        </Accordion>
-      </div>
-      <div className="mt-6">
-        <div className="mb-4">
-          <h3 className="text-lg font-semibold">อัตราดอกเบี้ยโบนัส</h3>
-        </div>
-        <div>
-          {bonusAccountLists.map((account) => {
-            const accountPref = accountPreferences.get(account.code)
-
-            return (
-              <div
-                key={account.code}
-                className="py-2 flex justify-between items-center"
-              >
-                <label
-                  htmlFor={`account-switch-${account.code}`}
-                  className={
-                    (accountPref?.bonus ?? false)
-                      ? 'text-primary'
-                      : 'text-gray-500'
-                  }
+              return (
+                <AccordionItem
+                  className="border-none"
+                  key={`advanced_filter_${bankCode}`}
+                  value={`advanced_filter_${bankCode}`}
                 >
-                  {account.shortName || account.name}
-                </label>
-                <Switch
-                  id={`account-switch-${account.code}`}
-                  checked={accountPref?.bonus ?? false}
-                  onCheckedChange={(checked) => {
-                    if (!accountPref) {
-                      setAccountPreferences(
-                        new Map(
-                          accountPreferences.set(account.code, {
-                            productCode: account.code,
-                            bonus: checked,
-                            enabled: true,
-                          }),
-                        ),
+                  <AccordionTrigger className="p-2 flex gap-2 border-b">
+                    <div className="flex items-center gap-2">
+                      <div
+                        className={cn(
+                          'size-6 rounded-full border shrink-0',
+                          !isBankEnable && 'grayscale',
+                        )}
+                        style={{
+                          backgroundColor: bank.icon?.bgColor,
+                        }}
+                      >
+                        {bank.icon && (
+                          <Image
+                            src={bank.icon.path}
+                            alt={bank.nameTh}
+                            width={40}
+                            height={40}
+                          />
+                        )}
+                      </div>
+                      <div
+                        className={cn(
+                          'flex flex-col',
+                          !isBankEnable && 'text-gray-500',
+                        )}
+                      >
+                        <span className="font-medium">{bank.nameTh}</span>
+                      </div>
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent className="p-0">
+                    {accounts.map((account) => {
+                      const accountPref = accountPreferences.get(account.code)
+
+                      return (
+                        <div
+                          key={account.code}
+                          className="p-2 flex justify-between items-center"
+                        >
+                          <label
+                            htmlFor={`account-switch-${account.code}`}
+                            className={
+                              (accountPref?.enabled ?? true)
+                                ? 'text-primary'
+                                : 'text-gray-500'
+                            }
+                          >
+                            {account.shortName || account.name}
+                          </label>
+                          <div>
+                            <Switch
+                              id={`account-switch-${account.code}`}
+                              checked={accountPref?.enabled ?? true}
+                              onCheckedChange={(checked) => {
+                                if (!accountPref) {
+                                  setAccountPreferences(
+                                    new Map(
+                                      accountPreferences.set(account.code, {
+                                        productCode: account.code,
+                                        enabled: checked,
+                                      }),
+                                    ),
+                                  )
+                                } else {
+                                  setAccountPreferences(
+                                    new Map(
+                                      accountPreferences.set(account.code, {
+                                        ...accountPref,
+                                        enabled: checked,
+                                      }),
+                                    ),
+                                  )
+                                }
+                              }}
+                            />
+                          </div>
+                        </div>
                       )
-                    } else {
-                      setAccountPreferences(
-                        new Map(
-                          accountPreferences.set(account.code, {
-                            ...accountPref,
-                            bonus: checked,
-                            enabled: true,
-                          }),
-                        ),
-                      )
-                    }
-                  }}
-                />
-              </div>
-            )
-          })}
+                    })}
+                  </AccordionContent>
+                </AccordionItem>
+              )
+            })}
+          </Accordion>
+        </div>
+        <div className="mt-6">
+          <div className="mb-4">
+            <h3 className="text-lg font-semibold">อัตราดอกเบี้ยโบนัส</h3>
+          </div>
+          <div>
+            {bonusAccountLists.map((account) => {
+              const accountPref = accountPreferences.get(account.code)
+
+              return (
+                <div
+                  key={account.code}
+                  className="py-2 flex justify-between items-center"
+                >
+                  <div className="align-center">
+                    <label
+                      htmlFor={`account-switch-${account.code}`}
+                      className={
+                        (accountPref?.bonus ?? false)
+                          ? 'text-primary'
+                          : 'text-gray-500'
+                      }
+                    >
+                      {account.shortName || account.name}
+                    </label>
+                    {!!account.bonusConditions && (
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Info className="ml-1 size-4" />
+                        </TooltipTrigger>
+                        <TooltipContent className="max-w-96">
+                          {account.bonusConditions}
+                        </TooltipContent>
+                      </Tooltip>
+                    )}
+                  </div>
+                  <Switch
+                    id={`account-switch-${account.code}`}
+                    checked={accountPref?.bonus ?? false}
+                    onCheckedChange={(checked) => {
+                      if (!accountPref) {
+                        setAccountPreferences(
+                          new Map(
+                            accountPreferences.set(account.code, {
+                              productCode: account.code,
+                              bonus: checked,
+                              enabled: true,
+                            }),
+                          ),
+                        )
+                      } else {
+                        setAccountPreferences(
+                          new Map(
+                            accountPreferences.set(account.code, {
+                              ...accountPref,
+                              bonus: checked,
+                              enabled: true,
+                            }),
+                          ),
+                        )
+                      }
+                    }}
+                  />
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </TooltipProvider>
   )
 })
 
