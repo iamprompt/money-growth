0e68f00270116400c19e71ecabff8cb9242774d5
Supakarn Laorattanakul <wearedprompt@gmail.com>
2024-07-21 01:21:28 +0700

    feat: add plausible analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Script from 'next/script'
 import { ReactNode } from 'react'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
@@ -10,6 +11,11 @@ const queryClient = new QueryClient()
 export const Providers = ({ children }: { children?: ReactNode }) => {
   return (
     <>
+      <Script
+        defer
+        data-domain="interests.iamprompt.me"
+        src="https://plausible.iamprompt.me/js/script.js"
+      />
       <TooltipProvider>
         <QueryClientProvider client={queryClient}>
           {children}
