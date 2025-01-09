6fb926a4d70568ae3e512a0e3ff94198acfc0ba3
Supakarn Laorattanakul <wearedprompt@gmail.com>
2025-01-09 21:48:30 +0700

    fix: remove plausible script

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import Script from 'next/script'
 import { ReactNode } from 'react'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
@@ -11,11 +10,6 @@ const queryClient = new QueryClient()
 export const Providers = ({ children }: { children?: ReactNode }) => {
   return (
     <>
-      <Script
-        defer
-        data-domain="interests.iamprompt.me"
-        src="https://plausible.iamprompt.me/js/script.js"
-      />
       <TooltipProvider>
         <QueryClientProvider client={queryClient}>
           {children}
